64231e0e0aa6b969e6db1a0a386ee7c6bc47d3ce
sin013 <158771236+sin013@users.noreply.github.com>
2024-08-10 20:41:19 +0300

    Add files via upload

---
+++ module 5.2.py
@@ -0,0 +1,29 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.floors = int(floors)
+
+    def go_to(self, new_floor):
+        int(new_floor)
+        if new_floor > self.floors or new_floor < 1:
+            print(f'В доме "{self.name}" этажа №{new_floor} не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.floors
+
+    def __str__(self):
+        return (f'Название: {self.name}, кол-во этажей: {self.floors}')
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
